766a5d2e05d7adac070198b2869cc2c01adbfc60
thesuprememexican2000 <darian321@outlook.com>
2023-03-22 03:25:30 -0400

    2 lines of code that took 5 seconds to write, please help

--- src/main/java/colval/edu/javase/QTC/BLL/Model/TaxBracket.java
+++ src/main/java/colval/edu/javase/QTC/BLL/Model/TaxBracket.java
@@ -1,4 +1,14 @@
 package colval.edu.javase.QTC.BLL.Model;
 
 public class TaxBracket {
+
+    private float min;
+    private float max;
+    private float rate;
+
+    public TaxBracket(float min, float max, float rate) {
+        this.min = min;
+        this.max = max;
+        this.rate = rate;
+    }
 }
