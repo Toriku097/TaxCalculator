eaec8a4f295be30f4af3237b35b2ebf686bcdfd2
thesuprememexican2000 <darian321@outlook.com>
2023-03-29 03:10:11 -0400

    more 2 second code

--- src/main/java/colval/edu/javase/QTC/BLL/Model/TaxBracket.java
+++ src/main/java/colval/edu/javase/QTC/BLL/Model/TaxBracket.java
@@ -6,9 +6,37 @@ public class TaxBracket {
     private float max;
     private float rate;
 
+    public TaxBracket() {
+
+    }
+
     public TaxBracket(float min, float max, float rate) {
         this.min = min;
         this.max = max;
+        this.rate = rate;
+    }
+
+    public float getMin() {
+        return min;
+    }
+
+    public void setMin(float min) {
+        this.min = min;
+    }
+
+    public float getMax() {
+        return max;
+    }
+
+    public void setMax(float max) {
+        this.max = max;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
         this.rate = rate;
     }
 }
